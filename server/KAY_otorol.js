9e474ceff1d01edbd3df8913f03edd7fd98eaef3
healthpackTR <68531011+healthpackTR@users.noreply.github.com>
2020-11-25 15:08:47 +0300

    Update KAY_otorol.js

--- server/KAY_otorol.js
+++ server/KAY_otorol.js
@@ -16,10 +16,16 @@ if(ne == "user"||ne == "kullanıcı") {
   if(role.position >= message.guild.member(client.user).roles.highest.position) return client.sendFalse(`${role}, Bu Rol Benim En Üst Rolümden Üstte Bir rol`,message.channel)
   await data.findOneAndUpdate({sunucu : message.guild.id},{$set : {"otoroll.bot":role.id}})
   client.sendTrue(`Otorol **Bot** Rolü ${role} Olarak başarıyla Ayarlandı`,message.channel)
-} else {
+} else if(ne == "kapat"){
+  let d = await data.findOne({sunucu : message.guild.id})
+  if(!d.otoroll.bot && !d.otoroll.user)return client.sendFalse(`Otorol Zaten Ayarlanmamış`,message.channel)
+  await data.findOneAndUpdate({sunucu : message.guild.id},{$set : {"otoroll.bot":null,"otoroll.user":null}})
+  client.sendTrue(`Otorol Sistemi başarıyla Kapatıldı`,message.channel)
+}else {
   const otorol = new Discord.MessageEmbed().setTitle('Otorol Sistem').setURL('https://discord.com/api/oauth2/authorize?client_id=769110620359622676&permissions=8&scope=bot')
   .addField(`<a:load:769855808879984660>__${p}otorol user @rol__`,`Otorol Gelen **Bot Olmayan** Kişilere Verilecek Rol`)
   .addField(`<a:load:769855808879984660>__${p}otorol bot @rol__`,'Otorol Gelen **Bot Olan** Kişilere Verilecek Rol')
+  .addField(`<a:load:769855808879984660>__${p}otorol kapat__`,`Sistemi Kapatır.`)
   .setFooter('Otorol Sistemi',message.author.avatarURL({dynamic : true})).setColor('#0e0c0c')
   message.channel.send(otorol)
 }
