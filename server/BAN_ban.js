f883177cae7e5aad4bc5e0b4a2165e8b4569a01d
healthpackTR <68531011+healthpackTR@users.noreply.github.com>
2020-11-25 12:06:47 +0300

    Update BAN_ban.js

--- server/BAN_ban.js
+++ server/BAN_ban.js
@@ -28,6 +28,7 @@ return client.sendFalse(`**Bugünkü Ban Limitini aştın! \`Ban\` Rolünü Alı
   if(!member)return client.sendFalse(`Banlamak İçin Bir Kullanıcı Etiketlemelisin`,message.channel)
   if(!sebep) return client.sendFalse(`Banlamak için Bir \`Sebep\` Yazmalısın`,message.channel)
   if(member.banable) return client.sendFalse(`Bu Kullanıcıyı Banlayamıyorum`,message.channel)
+  if(member.id == message.author.id) return message.reply('kendinden Ne istiyorsun :(')
   if(message.member.roles && member.roles && message.member.roles.highest.position <= member.roles.highest.position) return client.sendFalse(`Bu kullanıcı Aynı Pozisyona veya Senden Daha Üstte bir Role sahip`,message.channel)
 if(lm.bansistem.log) {
   const embed = new Discord.MessageEmbed()
