c3eb341fbfc53ea33e165d724b0b76e247f33a59
healthpackTR <68531011+healthpackTR@users.noreply.github.com>
2020-11-29 16:58:35 +0300

    Update KAY_isim.js

--- server/KAY_isim.js
+++ server/KAY_isim.js
@@ -10,7 +10,7 @@ let kız = f.Krol
 let user = message.mentions.users.first() || message.guild.members.cache.get(args[0])
 let member = message.guild.member(user)
 let ism = args.slice(1).join(" ")
-if(!message.member.roles.cache.has(yetkili)) return message.channel.send(new Discord.MessageEmbed().setDescription(`<a:olmaz:769202870612131840> | Kayıt Sorumlusu olan **<@&${yetkili}>** Rolüne Sahip Olmalısın!`).setColor(open.embedFalse))
+if(!message.member.roles.cache.has(yetkili) && !message.member.hasPermission('MANAGE_NICKNAMES')) return message.channel.send(new Discord.MessageEmbed().setDescription(`<a:olmaz:769202870612131840> | Kayıt Sorumlusu olan **<@&${yetkili}>** Rolüne Sahip Olmalısın!`).setColor(open.embedFalse))
 if(!member) return message.channel.send(new Discord.MessageEmbed().setDescription('<a:olmaz:769202870612131840> | Lütfen Bir **Kullanıcı @etiketleyin veya ID ** Yazınız..').setColor(open.embedFalse))
 if(!ism) return message.channel.send(new Discord.MessageEmbed().setDescription('<a:olmaz:769202870612131840> | Lütfen Yeni **İsimi** Giriniz.').setColor(open.embedFalse))
 if(!member.roles.cache.has(erkek) && !member.roles.cache.has(kız)) return message.channel.send(new Discord.MessageEmbed().setDescription(`<a:olmaz:769202870612131840> | Kullanıcının <@&${erkek}> Veya <@&${kız}> Rolüne Sahip olması gerekiyor.`).setColor(open.embedFalse))
