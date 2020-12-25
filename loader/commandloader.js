98efc05c2fe7470d25bd7e934a76901cfebcc8c5
healthpackTR <68531011+healthpackTR@users.noreply.github.com>
2020-12-25 23:10:28 +0300

    Update commandloader.js

--- loader/commandloader.js
+++ loader/commandloader.js
@@ -14,11 +14,11 @@ fs.readdir('./server/', (err, files) => {
     if (err) console.error(err);
     files.forEach(f => {
       if(!f.endsWith('.js'))return console.error(`${f}, Komutunun Sonu .js Olmadığı için Es Geçtim`)
-        let lord = require(`../server/${f}`);
-      if(!lord.help) return console.error(`${f}, Komutun => exports.help = {} <= Kısmı yok.`)
-        client.commands.set(lord.help.name, lord);
-        lord.help.aliases.forEach(alias => {
-            client.aliases.set(alias, lord.help.name);
+        let yasin = require(`../server/${f}`);
+      if(!yasin.help) return console.error(`${f}, Komutun => exports.help = {} <= Kısmı yok.`)
+        client.commands.set(yasin.help.name, yasin);
+        yasin.help.aliases.forEach(alias => {
+            client.aliases.set(alias, yasin.help.name);
         });
     });
   console.log(`${client.commands.size} Komut ve ${client.aliases.size} Alternatif Komut yüklendi.`)//help
